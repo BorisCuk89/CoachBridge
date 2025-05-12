bd5b5ccc331a5f02eea0c3715f6e1f6f5b597b4d
Boris Ćuk <boris@scuti.it>
2025-05-12 22:45:53 +0200

    Added style for welcome screen.

--- src/screens/auth/WelcomeScreen.tsx
+++ src/screens/auth/WelcomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
 import {useSelector, useDispatch} from 'react-redux';
 import {RootState} from '../../store/store.ts';
 
@@ -9,12 +9,23 @@ const WelcomeScreen = ({navigation}: any) => {
   console.log('isAuthenticate ', isAuthenticated);
   return (
     <View style={styles.container}>
+      <Image source={require('../../assets/logo.jpg')} style={styles.logo} />
+
       <Text style={styles.title}>Welcome to CoachBridge</Text>
-      <Button title="Login" onPress={() => navigation.navigate('Login')} />
-      <Button
-        title="Registracija"
-        onPress={() => navigation.navigate('ChooseRole')}
-      />
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Login')}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.button, styles.registerButton]}
+        onPress={() => navigation.navigate('ChooseRole')}>
+        <Text style={[styles.buttonText, styles.registerButtonText]}>
+          Registracija
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -26,10 +37,43 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#1b1a1a',
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    resizeMode: 'contain',
+    marginBottom: 0,
   },
   title: {
-    fontSize: 24,
+    fontSize: 32,
+    color: '#fff',
     fontWeight: 'bold',
+    marginBottom: 40,
+    textAlign: 'center',
+    width: 250,
+  },
+  button: {
+    backgroundColor: '#e2f069',
+    paddingVertical: 14,
+    paddingHorizontal: 40,
+    borderRadius: 15,
     marginBottom: 20,
+    width: '80%',
+    alignItems: 'center',
+  },
+  registerButton: {
+    backgroundColor: '#1b1a1a',
+    borderColor: '#fff',
+    borderStyle: 'solid',
+    borderWidth: '2',
+  },
+  buttonText: {
+    color: '#000',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  registerButtonText: {
+    color: '#fff',
   },
 });
