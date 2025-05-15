e8720738f67e1dc698c11d00c3a46b8dc6465691
Boris Ćuk <boris@scuti.it>
2025-05-15 22:34:22 +0200

    Added style for choose my role screen.

--- src/screens/auth/ChooseRoleScreen.tsx
+++ src/screens/auth/ChooseRoleScreen.tsx
@@ -1,15 +1,43 @@
 import React from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {useNavigation} from '@react-navigation/native';
 
-const ChooseRoleScreen = ({navigation}) => {
+const {width} = Dimensions.get('window');
+
+const ChooseRoleScreen = () => {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
+      {/* Strelica nazad */}
+      <TouchableOpacity
+        style={styles.backButton}
+        onPress={() => navigation.goBack()}>
+        <Ionicons name="arrow-back" size={24} color="#d8f24e" />
+      </TouchableOpacity>
+
+      {/* Naslov */}
       <Text style={styles.title}>Registracija</Text>
+
+      {/* Motivacioni tekst */}
+      <Text style={styles.subtitle}>
+        Kreiraj nalog kao klijent ili trener i postani deo fitnes zajednice.
+      </Text>
+
+      {/* Dugmad */}
       <TouchableOpacity
         style={styles.button}
         onPress={() => navigation.navigate('RegisterClient')}>
         <Text style={styles.buttonText}>Registruj se kao Klijent</Text>
       </TouchableOpacity>
+
       <TouchableOpacity
         style={styles.button}
         onPress={() => navigation.navigate('RegisterTrainer')}>
@@ -22,15 +50,42 @@ const ChooseRoleScreen = ({navigation}) => {
 export default ChooseRoleScreen;
 
 const styles = StyleSheet.create({
-  container: {flex: 1, justifyContent: 'center', alignItems: 'center'},
-  title: {fontSize: 24, fontWeight: 'bold', marginBottom: 20},
-  button: {
-    backgroundColor: '#007bff',
-    padding: 15,
-    borderRadius: 10,
-    marginBottom: 10,
-    width: 250,
+  container: {
+    flex: 1,
+    backgroundColor: '#1b1a1a',
+    paddingHorizontal: 24,
     alignItems: 'center',
+    justifyContent: 'center',
   },
-  buttonText: {color: '#fff', fontSize: 16, fontWeight: 'bold'},
+  backButton: {
+    position: 'absolute',
+    top: 105,
+    left: 20,
+  },
+  title: {
+    fontSize: 20,
+    color: '#d8f24e',
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subtitle: {
+    color: '#ccc',
+    fontSize: 14,
+    textAlign: 'center',
+    marginBottom: 30,
+    lineHeight: 20,
+  },
+  button: {
+    backgroundColor: '#fff',
+    paddingVertical: 16,
+    borderRadius: 28,
+    width: width - 48,
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  buttonText: {
+    color: '#1b1a1a',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
 });
