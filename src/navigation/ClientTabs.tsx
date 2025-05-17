953c7c02b6e17469bf593a74061f8b77e6d18fb7
Boris Ćuk <boris@scuti.it>
2025-05-17 20:13:47 +0200

    Added style for tabs.

--- src/navigation/ClientTabs.tsx
+++ src/navigation/ClientTabs.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/client/HomeScreen';
 import ClientProfileScreen from '../screens/client/ClientProfileScreen';
-import SettingsScreen from '../screens/client/SettingsScreen'; // možeš kasnije napraviti
+import SettingsScreen from '../screens/client/SettingsScreen';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
@@ -12,13 +12,27 @@ const ClientTabs = () => {
     <Tab.Navigator
       screenOptions={({route}) => ({
         headerShown: false,
+        tabBarStyle: {
+          backgroundColor: '#d8f24e',
+          height: 70,
+          borderTopWidth: 0,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: '600',
+          marginBottom: 6,
+        },
+        tabBarIconStyle: {
+          marginTop: 6,
+        },
+        tabBarActiveTintColor: '#1b1a1a',
+        tabBarInactiveTintColor: '#333',
         tabBarIcon: ({color, size}) => {
           let iconName = 'home';
           if (route.name === 'Home') iconName = 'home-outline';
           if (route.name === 'Profile') iconName = 'person-outline';
           if (route.name === 'Settings') iconName = 'settings-outline';
-
-          return <Icon name={iconName} size={size} color={color} />;
+          return <Icon name={iconName} size={22} color={color} />;
         },
       })}>
       <Tab.Screen name="Home" component={HomeScreen} />
